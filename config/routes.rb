8791c56904eaf393cb36c33026d7419fcd2cd6ee
Sergei Ianovich <ynvich@gmail.com>
2014-01-14 00:31:59 +0400

    user: merge `edit` operation into `show`

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ PcsWeb::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :users
+  resources :users, except: [:edit]
   resources :sessions, only: [:new, :create, :destroy]
   # You can have the root of your site routed with "root"
   root 'main#index'
