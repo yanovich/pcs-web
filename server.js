c1dd91ac297cae15e742cb6ff35faac57b3f5447
Sergei Ianovich <ynvich@gmail.com>
2014-01-30 01:23:51 +0400

    log: basic express.js logging

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ var path = require('path');
 
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(express.logger('dev'));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'asutp.io' });
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 // vim:ts=2 sts=2 sw=2 et:
