13d225d634b4850df6ec9a5cafb58593f17f3246
Sergei Ianovich <ynvich@gmail.com>
2014-03-19 23:03:50 +0400

    test for HTML escaping (closes #13)
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- test/views/user.js
+++ test/views/user.js
@@ -16,6 +16,8 @@ function t(key, options) {
 var User = require('../../models/user');
 var attrs = { name: "Example User1", email: "user-1@example.com",
       password: 'password', confirmation: 'password' }
+var attrs2 = { name: "a>bc",  email: "user-escape@example.com",
+      password: 'password', confirmation: 'password' }
 
 describe('User#show page', function(){
   var user;
@@ -47,4 +49,37 @@ describe('User#show page', function(){
   })
 });
 
+
+describe('HTML escaping of user input', function(){
+  var user;
+
+  before( function (done) {
+    user = new User(attrs2);
+    user.save(done);
+  });
+
+  beforeEach( function (done) {
+    browser
+    .pressButton(t('session.sign_out'))
+    .then(function () {
+      browser
+      .visit('/signin')
+      .then(function () {
+        browser
+        .fill(t('user.email'), attrs.email)
+        .fill(t('user.password'), attrs.password)
+        .pressButton(t('session.sign_in'))
+        .then(function () {
+          browser.visit('/users/' + user._id.toString()).then(done, done);
+        });
+      });
+    });
+  });
+
+  it('should display user', function(){
+    var doc = browser.document;
+    expect(browser.statusCode).to.be(200);
+    expect(browser.text('.tp-content > li')).to.contain(user.name);
+  })
+});
 // vim:ts=2 sts=2 sw=2 et:
