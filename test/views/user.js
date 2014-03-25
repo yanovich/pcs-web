b86917921913ee2f066fff9c75e07a385388ec47
Sergei Ianovich <ynvich@gmail.com>
2014-03-25 12:50:47 +0400

    reset browser before each integration test
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- test/views/user.js
+++ test/views/user.js
@@ -8,7 +8,7 @@
 var expect = require('expect.js');
 var Browser = require('zombie');
 
-var browser = new Browser({ site: global.url });
+var browser;
 function t(key, options) {
   return global.i18n.t(key, options);
 }
@@ -19,12 +19,20 @@ var attrs = { name: 'Example User1"', email: "user-1@example.com",
 
 var update = {};
 
-describe('User#show page', function(){
+describe('User', function(){
   var user;
 
-  before(function (done) {
+  beforeEach(function () {
+    browser = new Browser({ site: global.url });
+  })
+
+  before(function () {
     user = new User(attrs);
-    user.save(function () {
+    user.save();
+  });
+
+  describe('profile page', function () {
+    beforeEach(function (done) {
       browser
       .visit('/signin')
       .then(function () {
@@ -36,35 +44,35 @@ describe('User#show page', function(){
           browser.visit('/users/' + user._id.toString()).then(done, done);
         }, done);
       }, done);
-    });
-  });
-
-  it('should display user', function(){
-    expect(browser.statusCode).to.be(200);
-    expect(browser.query("input[value='"+user.name+"']")).to.be.true;
-    expect(browser.query('input[value="'+user.email+'"]')).to.be.true;
-  })
-
-  describe('edit with valid data', function () {
-    before(function (done) {
-      update.name = 'New Name';
-      update.email = 'new@example.com';
-      browser
-      .fill(t('user.name'), update.name)
-      .fill(t('user.email'), update.email)
-      .pressButton(t('user.update'))
-      .then(done, done);
     })
 
-    it('should show updated date', function (done) {
+    it('should display user', function () {
       expect(browser.statusCode).to.be(200);
-      expect(browser.query("input[value='"+update.name+"']")).to.be.true;
-      expect(browser.query('input[value="'+update.email+'"]')).to.be.true;
-      User.findById(user._id, function (err, user) {
-        user.name.should.equal(update.name);
-        user.email.should.equal(update.email);
-        done();
-      });
+      expect(browser.query("input[value='"+user.name+"']")).to.be.true;
+      expect(browser.query('input[value="'+user.email+'"]')).to.be.true;
+    })
+
+    describe('edit with valid data', function () {
+      before(function (done) {
+        update.name = 'New Name';
+        update.email = 'new@example.com';
+        browser
+        .fill(t('user.name'), update.name)
+        .fill(t('user.email'), update.email)
+        .pressButton(t('user.update'))
+        .then(done, done)
+      })
+
+      it('should show updated date', function (done) {
+        expect(browser.statusCode).to.be(200);
+        expect(browser.query("input[value='"+update.name+"']")).to.be.true;
+        expect(browser.query('input[value="'+update.email+'"]')).to.be.true;
+        User.findById(user._id, function (err, user) {
+          user.name.should.equal(update.name);
+          user.email.should.equal(update.email);
+          done();
+        });
+      })
     })
   })
 });
