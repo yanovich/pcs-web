2a2fd3162e1debb2f24ce0b586bd1caf66d1585e
Sergei Ianovich <ynvich@gmail.com>
2014-05-18 13:19:18 +0400

    ui: highlight active navigation link
    Signed-off-by: Sergei Ianovich <ynvich@gmail.com>

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,11 @@
 /* Controllers */
 
 angular.module('pcs.controllers', [])
+  .controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+      return viewLocation === '#' + $location.path();
+    }
+  }])
   .controller('SitesCtrl', ['$scope', function($scope) {
 
   }])
