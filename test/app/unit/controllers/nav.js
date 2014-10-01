3c195e0e9f97addc0f949d66195823396e34c29e
Sergey Gaichuk <sergey.gaychuk@gmail.com>
2014-10-01 03:54:17 +0300

    test: add angular nav's controller spec
    Signed-off-by: Sergey Gaichuk <sergey.gaychuk@gmail.com>

---
+++ test/app/unit/controllers/nav.js
@@ -0,0 +1,42 @@
+'use strict'
+/* test/app/unit/controllers/nav.js -- test Nav Angular controller
+ * Copyright 2014 Sergei Ianovich
+ *
+ * Licensed under AGPL-3.0 or later, see LICENSE
+ * Process Control Service Web Interface
+ */
+
+describe("Nav Controllers", function() {
+  var httpBackend;
+
+  beforeEach(function() {
+    angular.mock.module('pcs.services');
+    angular.mock.module('pcs.controllers');
+  });
+
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend;
+  }));
+
+  afterEach(function(){
+    httpBackend.verifyNoOutstandingExpectation();
+  });
+
+  describe("NavCtrl", function() {
+    var scope, location, controller;
+
+    beforeEach(inject(function($location, $controller) {
+      location = $location;
+      controller = $controller;
+      scope = {};
+    }));
+
+    it("should define isActive", function() {
+      location.path("/aaa");
+      controller('NavCtrl', { $scope: scope });
+      expect(scope.isActive).toBeDefined();
+      expect(scope.isActive("#/aaa")).toBeTruthy();
+    });
+  });
+});
+
