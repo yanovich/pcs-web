c110c92dbaf4bea31d784417fccdcf8d3ac217e1
Sergei Ianovich <s@asutp.io>
2014-12-24 00:03:09 +0300

    tests: ajax system service
    Signed-off-by: Sergei Ianovich <s@asutp.io>

---
+++ test/browser/system_service_test.js
@@ -0,0 +1,29 @@
+/* test/browser/system_service_test.js -- test System Angular service
+ * Copyright 2014 Sergei Ianovich
+ *
+ * Licensed under AGPL-3.0 or later, see LICENSE
+ * Process Control Service Web Interface
+ */
+'use strict';
+
+describe("System Service", function() {
+  var service;
+
+  beforeEach(function() {
+    service = {};
+    service.resource = sinon.spy();
+    angular.mock.module('pcs.services', function($provide) {
+      $provide.value('$resource', service.resource);
+    });
+  });
+
+  it("should create resource", function() {
+    inject(function(System) {
+      expect(service.resource).to.have.been.calledWith(
+          '/sites/:siteId/systems/:systemId',
+          { siteId: '@site', systemId: '@_id' }
+          );
+    });
+  });
+});
+// vim:ts=2 sts=2 sw=2 et:
