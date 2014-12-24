fa42b87105c23ff377ec22831ee67577fd7fcbd9
Sergei Ianovich <s@asutp.io>
2014-12-24 03:14:31 +0300

    tests: ajax setpoints service
    Signed-off-by: Sergei Ianovich <s@asutp.io>

---
+++ test/browser/setpoints_service_test.js
@@ -0,0 +1,25 @@
+/* test/browser/setpoints_service_test.js -- test Device Setpoints Angular service
+ * Copyright 2014 Sergei Ianovich
+ *
+ * Licensed under AGPL-3.0 or later, see LICENSE
+ * Process Control Service Web Interface
+ */
+'use strict';
+
+describe("Setpoints Service", function() {
+  var resource;
+
+  beforeEach(function() {
+    resource = sinon.spy();
+    angular.mock.module('pcs.services', function($provide) {
+      $provide.value('$resource', resource);
+    });
+  });
+
+  it("should create resource", function() {
+    inject(function(Setpoints) {
+      expect(resource).to.have.been.calledWith('/devices/:deviceId/setpoints');
+    });
+  });
+});
+// vim:ts=2 sts=2 sw=2 et:
