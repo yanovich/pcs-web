ad57d0a6c4b2ae9cf46f2f6c3f27115737c4f696
Sergei Ianovich <s@asutp.io>
2014-12-24 03:35:00 +0300

    tests: ajax: pager properties
    Signed-off-by: Sergei Ianovich <s@asutp.io>

---
+++ test/browser/page_controller_test.js
@@ -0,0 +1,45 @@
+/* test/app/unit/controllers/page.js -- test Page Angular controller
+ * Copyright 2014 Sergei Ianovich
+ *
+ * Licensed under AGPL-3.0 or later, see LICENSE
+ * Process Control Service Web Interface
+ */
+'use strict';
+
+describe("Page Controller", function() {
+  var httpBackend;
+
+  beforeEach(function() {
+    angular.mock.module('pcs.services');
+    angular.mock.module('pcs.controllers');
+  });
+
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend;
+  }));
+
+  afterEach(function(){
+    httpBackend.verifyNoOutstandingExpectation();
+  });
+
+  describe("PageCtrl", function() {
+    var scope, location, controller;
+
+    beforeEach(inject(function($location, $controller) {
+      location = $location;
+      controller = $controller;
+      scope = {};
+    }));
+
+    it("should define pager", function() {
+      controller('PageCtrl', { $scope: scope });
+      expect(scope.pager).to.exist();
+      expect(scope.pager.first).to.equal(0);
+      expect(scope.pager.last).to.equal(0);
+      expect(scope.pager.count).to.equal(0);
+      expect(scope.pager.page).to.equal(1);
+      expect(scope.pager.show).to.not.be.true;
+    });
+  });
+});
+// vim:ts=2 sts=2 sw=2 et:
