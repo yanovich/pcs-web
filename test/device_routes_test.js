fb9654103d36bc41da9a8563b27871f296d2ae36
Sergey Gaichuk <sergey.gaychuk@gmail.com>
2014-12-24 18:29:38 +0300

    tests: routes: check device loaded and placed to request object
    Signed-off-by: Sergey Gaichuk <sergey.gaychuk@gmail.com>
    Signed-off-by: Sergei Ianovich <s@asutp.io>

---
+++ test/device_routes_test.js
@@ -0,0 +1,41 @@
+/* test/device_routes_test.js -- test Device routes
+ * Copyright 2014 Sergei Ianovich
+ *
+ * Licensed under AGPL-3.0 or later, see LICENSE
+ * Process Control Service Web Interface
+ */
+
+var expect = require('expect.js');
+var Device = require('../models/device');
+var Routes = require('../routes/device');
+
+var deviceAttrs = {
+  name: "Some device",
+};
+
+describe('Device routes', function() {
+  var admin;
+  beforeEach(function(done) {
+    Device.find().remove(function() {
+      (new Device(deviceAttrs)).save(function(err, newDevice) {
+        if (err) throw err;
+        device = newDevice;
+        return done();
+      });
+    });
+  });
+
+  describe("#load", function() {
+    it("should find by id and assign device to req", function(done) {
+      var req = { },
+      id = device._id;
+
+      Routes.load(req, {}, function() {
+        expect(req.device.toJSON()).to.eql(device.toJSON());
+        done();
+      }, id);
+    });
+  });
+});
+
+// vim:ts=2 sts=2 sw=2 et:
