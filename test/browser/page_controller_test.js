eafa9808423abd8ee2d9c6aa98907213d2932695
Sergei Ianovich <s@asutp.io>
2014-12-24 03:38:25 +0300

    tests: ajax: operator on page
    Signed-off-by: Sergei Ianovich <s@asutp.io>

--- test/browser/page_controller_test.js
+++ test/browser/page_controller_test.js
@@ -29,10 +29,10 @@ describe("Page Controller", function() {
       location = $location;
       controller = $controller;
       scope = {};
+      controller('PageCtrl', { $scope: scope });
     }));
 
     it("should define pager", function() {
-      controller('PageCtrl', { $scope: scope });
       expect(scope.pager).to.exist();
       expect(scope.pager.first).to.equal(0);
       expect(scope.pager.last).to.equal(0);
@@ -42,8 +42,11 @@ describe("Page Controller", function() {
     });
 
     it("should define moment", function() {
-      controller('PageCtrl', { $scope: scope });
       expect(scope.moment).to.exist();
+    });
+
+    it("should define operator", function() {
+      expect(scope.operator).to.eql({});
     });
   });
 });
