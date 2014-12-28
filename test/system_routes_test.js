04372cd4447dc59b158ea1fb44945bb3185fc116
Sergei Ianovich <s@asutp.io>
2014-12-28 21:41:00 +0300

    tests: routes: update system authentication
    Signed-off-by: Sergei Ianovich <s@asutp.io>

--- test/system_routes_test.js
+++ test/system_routes_test.js
@@ -94,6 +94,17 @@ describe('System routes', function() {
       router(Routes.setpoints, req, res);
     });
   });
+
+  describe("#update", function() {
+    it("should deny access to non-signed-in users", function(done) {
+      var req = { session: {} },
+      res = { redirect: function(url) {
+        expect(url).to.eql("/signin");
+        done();
+      }};
+      router(Routes.update, req, res);
+    });
+  });
 });
 
 // vim:ts=2 sts=2 sw=2 et:
